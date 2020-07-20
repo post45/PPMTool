beb1197af65af889ee829ec50cb8fec78eb0fc87
post45 <bashkuresh92@yandex.ru>
2020-07-20 15:38:42 -0400

    Second Commit

--- src/main/java/io/nabiullin/ppmtool/domain/Project.java
+++ src/main/java/io/nabiullin/ppmtool/domain/Project.java
@@ -1,4 +1,54 @@
 package io.nabiullin.ppmtool.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+
+    private String projectIdentifier;
+
+    private String discription ;
+
+    private String start_date;
+
+    private String end_date;
+
+
+    private Date created_At;
+
+    private Date updated_At;
+
+    public Project() {
+    }
+
+    public Project(String projectName,
+                   String projectIdentifier,
+                   String discription,
+                   String start_date,
+                   String end_date,
+                   Date created_At,
+                   Date updated_At) {
+        this.projectName = projectName;
+        this.projectIdentifier = projectIdentifier;
+        this.discription = discription;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.created_At = created_At;
+        this.updated_At = updated_At;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
 }
---
+++ src/main/java/io/nabiullin/ppmtool/web/ProjectController.java
@@ -0,0 +1,26 @@
+package io.nabiullin.ppmtool.web;
+
+import io.nabiullin.ppmtool.domain.Project;
+import io.nabiullin.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+        return new ResponseEntity<Project>(project,HttpStatus.CREATED);
+    }
+
+
+}
