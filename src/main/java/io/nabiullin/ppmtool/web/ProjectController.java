62625099c6319efa8e58ed1b44cc3d78aab3e419
post45 <bashkuresh92@yandex.ru>
2020-07-21 13:22:54 -0400

    Third commit

--- src/main/java/io/nabiullin/ppmtool/web/ProjectController.java
+++ src/main/java/io/nabiullin/ppmtool/web/ProjectController.java
@@ -1,17 +1,20 @@
 package io.nabiullin.ppmtool.web;
 
 import io.nabiullin.ppmtool.domain.Project;
+import io.nabiullin.ppmtool.services.MapValidationErrorService;
 import io.nabiullin.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,12 +23,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Proj Obj",HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?>errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap!=null)return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project,HttpStatus.CREATED);
