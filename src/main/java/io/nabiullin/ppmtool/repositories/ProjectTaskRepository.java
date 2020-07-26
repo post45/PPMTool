7516161edf3661a38b0921c35fb8f964f667febd
post45 <bashkuresh92@yandex.ru>
2020-07-25 20:51:18 -0400

    Backlog commit

---
+++ src/main/java/io/nabiullin/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package io.nabiullin.ppmtool.repositories;
+
+import io.nabiullin.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask,Long> {
+}
