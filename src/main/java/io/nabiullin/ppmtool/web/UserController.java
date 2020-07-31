c7ed4918dc47b4f9cb1eb0b5408092a986b18b39
post45 <bashkuresh92@yandex.ru>
2020-07-31 11:42:52 -0400

    Token created commit

--- src/main/java/io/nabiullin/ppmtool/web/UserController.java
+++ src/main/java/io/nabiullin/ppmtool/web/UserController.java
@@ -1,16 +1,25 @@
 package io.nabiullin.ppmtool.web;
 
 import io.nabiullin.ppmtool.domain.User;
+import io.nabiullin.ppmtool.payload.JWTLoginSuccessResponse;
+import io.nabiullin.ppmtool.payload.LoginRequest;
+import io.nabiullin.ppmtool.security.JwtTokenProvider;
 import io.nabiullin.ppmtool.services.MapValidationErrorService;
 import io.nabiullin.ppmtool.services.UserService;
 import io.nabiullin.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static io.nabiullin.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,6 +34,32 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+    ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+    if (errorMap != null) return  errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true,jwt));
+
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
